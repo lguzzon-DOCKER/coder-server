b937475ea62c6a634f4fef78d0ace5d398be5b79
Luca Guzzon <luca.guzzon@gmail.com>
2020-02-07 17:05:50 +0100

    Update deploy.sh

--- scripts/travis/deploy.sh
+++ scripts/travis/deploy.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+readonly TRAVIS_REPO_OWNER=${TRAVIS_REPO_SLUG%/*}
+readonly TRAVIS_REPO_NAME=${TRAVIS_REPO_SLUG#*/}
 
-docker login -u "$DOCKER_USER" -p "$DOCKER_PASS"
+echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin
 
-docker tag  "$BUILD_TAG" "$DOCKER_USER/code-server:latest"
-docker push "$DOCKER_USER/code-server:latest"
+docker tag  "$BUILD_TAG" "$DOCKER_USER/$TRAVIS_REPO_NAME:latest"
+docker push "$DOCKER_USER/$TRAVIS_REPO_NAME:latest"
+
+docker tag  "$BUILD_TAG" "$DOCKER_USER/$TRAVIS_REPO_NAME:$TRAVIS_TAG"
+docker push "$DOCKER_USER/$TRAVIS_REPO_NAME:$TRAVIS_TAG"
